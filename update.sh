26761c7cd0025b64f5422a9063c490a84be26acc
Thomas <7172001+pizza-enthusiast@users.noreply.github.com>
2019-10-20 16:39:17 -0700

    making the script less noisy
    the script is less noisy now!

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 cd ~/Desktop
 curl https://raw.githubusercontent.com/pizza-enthusiast/flopper/master/neti.gif > teapot0.gif
-tee teapot{1..420}.gif < teapot0.gif
+tee teapot{1..420}.gif < teapot0.gif >/dev/null
